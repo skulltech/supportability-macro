f4ea822b16f094003e5a039129ce185afa9acf18
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-09-10 04:52:45 +0530

    Add an import statement I forgot to add.

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
